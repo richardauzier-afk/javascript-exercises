540421d3e9b3d353016f84c15e4cd85b49a3a761
Richard Auzier <rlla@icomp.ufam.edu.br>
2025-05-31 22:08:22 -0400

    One more

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+	let stringAdded = "";
+	if(num < 0){
+		return "ERROR";
+	}
+	for(let i = 1; i <= num; i++){
+		stringAdded += string;
+	}
+	return stringAdded;
 
 };
+
+console.log(repeatString("hello",10));
 
 // Do not edit below this line
 module.exports = repeatString;
