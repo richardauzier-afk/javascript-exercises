84e607723868b532d5494654b00f725706b7fcdd
Richard Auzier <rlla@icomp.ufam.edu.br>
2025-06-01 22:00:53 -0400

    Done

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const DIVISIBLE_BY_FOUR = 4;
+const DIVISIBLE_BY_HUNDRED = 100;
+const DIVISIBLE_BY_FOUR_HUNDRED = 400;
 
+
+
+  
+const leapYears = function(year) {
+	if((year % DIVISIBLE_BY_HUNDRED === 0) && (year % DIVISIBLE_BY_FOUR_HUNDRED === 0)) return true;
+	else if((year % DIVISIBLE_BY_FOUR === 0) && !(year % DIVISIBLE_BY_HUNDRED === 0)) return true;
+	else return false; 
 };
 
 // Do not edit below this line
