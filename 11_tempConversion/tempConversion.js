a08f56bdd41277ec39330f6c79db23b34acb8fa6
Richard Auzier <rlla@icomp.ufam.edu.br>
2025-06-01 22:14:50 -0400

    Done

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+	return +((temperature - 32) * (5/9)).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+	return +((temperature * 1.8) + 32).toFixed(1);
 };
 
 // Do not edit below this line
