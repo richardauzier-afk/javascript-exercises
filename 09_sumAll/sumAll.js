5bc7d86712c23088f4a2e4b5712aca4f6f453ff4
Richard Auzier <rlla@icomp.ufam.edu.br>
2025-06-01 21:17:02 -0400

    Done

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+	let sum = 0;
+	
+	if(start > end){
+		let temp = start;
+		start = end;
+		end = temp;	
+	}
+	if(start < 0 || end < 0){
+		return 'ERROR';
+	}
+	if(!Number.isInteger(start) || !Number.isInteger(end)){
+		return 'ERROR'; 
+	}
+	if(!typeof start == 'number' || !typeof end == 'number'){
+		return 'ERROR';
 
+	}
+	for(let i = start; start <=end; start++){
+		sum += start;	
+	}
+
+	return sum;
 };
 
 // Do not edit below this line
